8a64c3a8be01010d8cc563a1653829522d98f1b0
Ashok Saravanan M <mani.ashoksaravanan@gmail.com>
2016-06-24 08:45:24 +0530

    Changed Versions.

--- app/src/main/java/com/ashoksm/pinfinder/sqlite/BankBranchSQLiteHelper.java
+++ app/src/main/java/com/ashoksm/pinfinder/sqlite/BankBranchSQLiteHelper.java
@@ -22,7 +22,7 @@ public class BankBranchSQLiteHelper extends SQLiteOpenHelper {
     private static final String CLASS_NAME = BankBranchSQLiteHelper.class.getName();
 
     // Database Version
-    private static final int DATABASE_VERSION = 10;
+    private static final int DATABASE_VERSION = 11;
 
     // Database Name
     private static final String DATABASE_NAME = "ashoksm.bankbranch";
