e51c92f848221719c3e819c7ebe4ec01a5f2cb0c
Ashok Saravanan M <mani.ashoksaravanan@gmail.com>
2017-03-14 12:58:12 +0530

    IFSC db updated as per FEB 2017 RBI info

--- app/src/main/java/com/ashoksm/pinfinder/sqlite/BankSQLiteHelper.java
+++ app/src/main/java/com/ashoksm/pinfinder/sqlite/BankSQLiteHelper.java
@@ -21,7 +21,7 @@ public class BankSQLiteHelper extends SQLiteOpenHelper {
     private static final String CLASS_NAME = BankSQLiteHelper.class.getName();
 
     // Database Version
-    private static final int DATABASE_VERSION = 18;
+    private static final int DATABASE_VERSION = 19;
 
     // Database Name
     private static final String DATABASE_NAME = "ashoksm.bankbranch";
