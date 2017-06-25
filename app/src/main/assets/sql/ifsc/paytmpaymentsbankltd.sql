9153d332179d26c1eb2dab81ca396b0808b48142
Ashok Saravanan M <mani.ashoksaravanan@gmail.com>
2017-06-25 10:59:56 +0530

    IFSC DB updated June 25

--- app/src/main/assets/sql/ifsc/paytmpaymentsbankltd.sql
+++ app/src/main/assets/sql/ifsc/paytmpaymentsbankltd.sql
@@ -1,1 +1,1 @@
-INSERT INTO bank_branch_t VALUES('Noida Branch', 'Noida', 'B-121, Sector-5', '2147483647', 'NA', 'PYTM0000001', 14660);
+INSERT INTO bank_branch_t VALUES('Noida Branch', 'Noida', 'B-121, Sector-5', '2147483647', 'NA', 'PYTM0000001', 14518);
